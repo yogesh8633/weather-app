c6b0bbe4c016e6d99c71c6d50d9755c2a1fa712f
yogesh8633 <yogeshg9711@gmail.com>
2024-08-27 19:59:56 +0530

    eslint error removed

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -22,7 +22,7 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({
   rain_accumulation,
 }) => {
   return (
-    <div className="bg-gray-800 text-white p-6 rounded-lg max-w-lg mx-auto mt-8 shadow-lg mb-5">
+    <div className="bg-[#171717] text-white p-6 rounded-lg max-w-xl mx-auto mt-8 shadow-lg mb-5">
       <div className="text-center mb-4">
         <h1 className="text-3xl font-bold">{city}</h1>
         <p className="text-xl">{locality}</p>
@@ -34,36 +34,36 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({
         </div>
       </div>
       <div className="grid grid-cols-2 gap-4">
-        <div className="flex items-center bg-gray-700 p-4 rounded-lg">
-          <i className="fas fa-tint text-2xl mr-4"></i>
+        <div className="flex items-center bg-[#1e1e1e] p-4 rounded-lg">
+          <i className="mdi mdi-water-percent text-2xl mr-4"></i>
           <div>
             <p className="text-xs">Humidity</p>
             <p>{humidity !== null ? `${humidity}%` : "N/A"}</p>
           </div>
         </div>
-        <div className="flex items-center bg-gray-700 p-4 rounded-lg">
-          <i className="fas fa-wind text-2xl mr-4"></i>
+        <div className="flex items-center bg-[#1e1e1e] p-4 rounded-lg">
+          <i className="mdi mdi-weather-windy text-2xl mr-4"></i>
           <div>
             <p className="text-xs">Wind Speed</p>
             <p>{wind_speed !== null ? `${wind_speed} m/s` : "N/A"}</p>
           </div>
         </div>
-        <div className="flex items-center bg-gray-700 p-4 rounded-lg">
-          <i className="fas fa-compass text-2xl mr-4"></i>
+        <div className="flex items-center bg-[#1e1e1e] p-4 rounded-lg">
+          <i className="mdi mdi-compass-outline text-2xl mr-4"></i>
           <div>
             <p className="text-xs">Wind Direction</p>
             <p>{wind_direction !== null ? `${wind_direction}°` : "N/A"}</p>
           </div>
         </div>
-        <div className="flex items-center bg-gray-700 p-4 rounded-lg">
-          <i className="fas fa-cloud-rain text-2xl mr-4"></i>
+        <div className="flex items-center bg-[#1e1e1e] p-4 rounded-lg">
+          <i className="mdi mdi-weather-pouring text-2xl mr-4"></i>
           <div>
             <p className="text-xs">Rain Intensity</p>
             <p>{rain_intensity !== null ? `${rain_intensity} mm/h` : "N/A"}</p>
           </div>
         </div>
-        <div className="flex items-center bg-gray-700 p-4 rounded-lg">
-          <i className="fas fa-water text-2xl mr-4"></i>
+        <div className="flex items-center bg-[#1e1e1e] p-4 rounded-lg">
+          <i className="mdi mdi-water text-2xl mr-4"></i>
           <div>
             <p className="text-xs">Rain Accumulation</p>
             <p>
