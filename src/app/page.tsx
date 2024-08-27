f7abac2adae8cce73bac73fc3c05b0c9c5d09ad6
yogesh8633 <yogeshg9711@gmail.com>
2024-08-27 17:23:26 +0530

    added weather display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { useAppDispatch } from '../hooks/useAppDispatch';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import LocalitySearch from "@/components/SearchBar";
+import { useAppDispatch } from "../hooks/useAppDispatch";
 import { useAppSelector } from "../hooks/useAppSelector";
-import LocalitySearch from '@/components/SearchBar';
+import WeatherDetails from "@/components/WeatherDisplay";
 
 export default function Home() {
   const dispatch = useAppDispatch();
   const weatherData = useAppSelector((state) => state.weather.weatherData);
   const weatherStatus = useAppSelector((state) => state.weather.status);
   const [localities, setLocalities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState<string | null>(null);
+  const [selectedLocality, setSelectedLocality] = useState<string | null>(null);
+
+  const handleLocalitySelected = (cityName: string, localityName: string) => {
+    setSelectedCity(cityName);
+    setSelectedLocality(localityName);
+  };
 
   useEffect(() => {
     const fetchLocalities = async () => {
       try {
-        const response = await fetch('/api/parsePdf');
+        const response = await fetch("/api/parsePdf");
         const data = await response.json();
         setLocalities(data);
       } catch (error) {
-        console.error('Failed to fetch localities:', error);
+        console.error("Failed to fetch localities:", error);
       }
     };
 
@@ -26,35 +35,101 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen items-center justify-between bg-white text-black">
-      {/* Main Content */}
-      <main className="flex flex-col items-center justify-center flex-grow">
-        <img
-          src="/google_logo.png" // Replace with the correct path to the Google logo
+    <>
+      <nav className="flex justify-end items-center mx-4 mt-14">
+        <a href="#" className="text-base text-black mr-3">
+          Gmail
+        </a>
+        <a href="#" className="text-base text-black mr-3">
+          Images
+        </a>
+        <Image
+          src="/menu.png"
+          className="w-8 h-8 p-1 cursor-pointer hover:opacity-80"
+          alt="Menu"
+          width={20}
+          height={20}
+        />
+        <button className="ml-3 py-2 px-4 border-2 border-blue-500 rounded-lg bg-blue-500 text-white font-semibold hover:bg-blue-600 focus:outline-none">
+          Sign in
+        </button>
+      </nav>
+
+      <section className="flex flex-col items-center justify-center flex-1 w-full px-4">
+        <Image
+          src="/google.png"
+          className={`block w-40 mt-20 mb-10 ${weatherData ? "mt-10" : ""}`}
           alt="Google Logo"
-          className="w-[272px] h-auto mt-16 mb-8"
+          width={500}
+          height={500}
         />
 
-        <div className="w-full max-w-xl">
-          <LocalitySearch localities={localities} />
-
-          
+        <div className="w-full items-center flex flex-col">
+          <LocalitySearch
+            localities={localities}
+            onLocalitySelected={handleLocalitySelected}
+          />
         </div>
-      </main>
 
-      {/* Footer */}
-      <footer className="w-full bg-gray-100 py-4">
-        <div className="flex justify-center space-x-4 text-gray-700 text-sm">
-          <a href="#" className="hover:underline">Advertising</a>
-          <a href="#" className="hover:underline">Business</a>
-          <a href="#" className="hover:underline">How Search works</a>
-        </div>
-        <div className="flex justify-center mt-2 space-x-4 text-gray-700 text-sm">
-          <a href="#" className="hover:underline">Privacy</a>
-          <a href="#" className="hover:underline">Terms</a>
-          <a href="#" className="hover:underline">Settings</a>
+        {/* Conditionally render WeatherDetails based on weatherData */}
+        {weatherStatus === "loading" && <p>Loading...</p>}
+        {weatherStatus === "succeeded" && weatherData && (
+          <WeatherDetails
+            city={selectedCity}
+            locality={selectedLocality}
+            temperature={weatherData.temperature}
+            humidity={weatherData.humidity}
+            wind_speed={weatherData.wind_speed}
+            wind_direction={weatherData.wind_direction}
+            rain_intensity={weatherData.rain_intensity}
+            rain_accumulation={weatherData.rain_accumulation}
+          />
+        )}
+        {weatherStatus === "failed" && <p>Error fetching weather data.</p>}
+
+        {/* <div className="mt-8 text-sm text-gray-700">
+          Google offered in:
+          <a href="#" className="text-blue-600 ml-1 hover:underline">
+            Nederlands
+          </a>
+          <a href="#" className="text-blue-600 ml-1 hover:underline">
+            Frysk
+          </a>
+        </div> */}
+      </section>
+
+      <footer className="bg-gray-200 py-4">
+        <h4 className="text-gray-600 text-base font-normal pl-4 border-l-4 border-gray-300 text-center">
+          India
+        </h4>
+        <div className="flex w-100 flex-wrap">
+          <div className="flex-1 pl-6">
+            <a href="#" className="text-gray-700 text-sm hover:underline">
+              Advertising
+            </a>
+            <a href="#" className="text-gray-700 text-sm ml-2 hover:underline">
+              Business
+            </a>
+            <a href="#" className="text-gray-700 text-sm ml-2 hover:underline">
+              About
+            </a>
+            <a href="#" className="text-gray-700 text-sm ml-2 hover:underline">
+              How Search works
+            </a>
+          </div>
+          <div className="flex-1 text-right pr-6 flex-wrap">
+            <a href="#" className="text-gray-700 text-sm hover:underline">
+              Privacy
+            </a>
+            <a href="#" className="text-gray-700 text-sm ml-2 hover:underline">
+              Terms
+            </a>
+            <a href="#" className="text-gray-700 text-sm ml-2 hover:underline">
+              Settings
+            </a>
+          </div>
         </div>
       </footer>
-    </div>
+    </>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,10 @@
+"use client"; // Ensure this is at the very top
+
 import React, { useState } from "react";
 import Autosuggest from "react-autosuggest";
-import { useAppDispatch } from "../hooks/useAppDispatch"; // Import your dispatch hook
-import { getWeatherByLocalityId } from "../features/weather/WeatherSlice"; // Import the Redux action
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import Image from "next/image";
+import { getWeatherByLocalityId } from "@/features/weather/WeatherSlice";
 
 interface Locality {
   cityName: string;
@@ -11,8 +14,12 @@ interface Locality {
   longitude: number;
   device_type: string;
 }
+interface SearchBarProps {
+  localities: Locality[];
+  onLocalitySelected: (cityName: string, localityName: string) => void; // Add this prop
+}
 
-const SearchBar: React.FC<{ localities: Locality[] }> = ({ localities }) => {
+const SearchBar: React.FC<SearchBarProps> = ({ localities, onLocalitySelected }) => {
   const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState<Locality[]>([]);
   const dispatch = useAppDispatch(); // Initialize dispatch
@@ -52,14 +59,26 @@ const SearchBar: React.FC<{ localities: Locality[] }> = ({ localities }) => {
 
   const onSuggestionSelected = (event: any, { suggestion }: any) => {
     const localityId = suggestion.localityId;
-    dispatch(getWeatherByLocalityId(localityId)); // Dispatch the action to fetch weather data
+    dispatch(getWeatherByLocalityId(localityId)); // Fetch the weather data for the selected locality
+
+    // Pass the city and locality names to the parent component
+    onLocalitySelected(suggestion.cityName, suggestion.localityName);
   };
 
   const handleSearch = () => {
     if (suggestions.length > 0) {
       const firstSuggestion = suggestions[0];
-      dispatch(getWeatherByLocalityId(firstSuggestion.localityId));
+      dispatch(getWeatherByLocalityId(firstSuggestion.localityId)); // Trigger search for the first suggestion
+
+      // Pass the city and locality names to the parent component
+      onLocalitySelected(firstSuggestion.cityName, firstSuggestion.localityName);
     }
+  };
+
+  const clearInput = () => {
+    setValue("");
+    setSuggestions([]);
+    
   };
 
   const inputProps = {
@@ -67,44 +86,70 @@ const SearchBar: React.FC<{ localities: Locality[] }> = ({ localities }) => {
     value,
     onChange,
     className:
-      "w-full px-4 py-2 border-none rounded-l-full focus:outline-none text-gray-800",
+      "w-full px-12 py-3 border border-gray-300 rounded-full focus:outline-none focus:border-gray-400 focus:ring-1 focus:ring-gray-400",
   };
 
   return (
     <>
-    <div className="w-full flex items-center bg-white border border-gray-300 rounded-full shadow-md hover:shadow-lg transition-shadow duration-200 max-w-xl">
-      <Autosuggest
-        suggestions={suggestions}
-        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={onSuggestionsClearRequested}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
-        onSuggestionSelected={onSuggestionSelected}
-        inputProps={inputProps}
-        theme={{
-          container: "relative w-full",
-          suggestionsContainer:
-            "absolute mt-1 w-full bg-white rounded-lg shadow-lg z-10",
-          suggestionsList: "list-none p-0 m-0",
-          suggestion: "py-2 px-4 hover:bg-gray-200 cursor-pointer",
-          suggestionHighlighted: "bg-gray-200",
-        }}
-      />
-    </div>
-      <div className="flex mt-8 space-x-2 justify-center">
+      <div className="relative w-full max-w-xl mt-2">
+        <Image
+          src="/search.svg"
+          className="absolute left-4 top-4 w-5 opacity-40"
+          alt="Search icon"
+          width={20}
+          height={20}
+        />
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          onSuggestionSelected={onSuggestionSelected}
+          inputProps={inputProps}
+          theme={{
+            container: "relative w-full",
+            suggestionsContainer:
+              "absolute mt-1 w-full bg-white rounded-lg shadow-lg z-10 max-h-60 overflow-y-auto", // Scrollable container with max height
+            suggestionsList: "list-none p-0 m-0",
+            suggestion: "py-2 px-4 hover:bg-gray-200 cursor-pointer",
+            suggestionHighlighted: "bg-gray-200",
+          }}
+        />
+        {value && (
+          <Image
+            src="/close.png" // Replace with your actual clear icon path
+            className="absolute right-6 top-4 w-3 cursor-pointer"
+            alt="Clear icon"
+            width={20}
+            height={20}
+            onClick={clearInput}
+          />
+        )}
+        <Image
+          src="/vs.png"
+          className="absolute right-12 top-4 w-3"
+          alt="Voice search icon"
+          width={20}
+          height={20}
+        />
+      </div>
+      <div className="flex justify-center mt-6 space-x-2">
         <button
           type="submit"
           onClick={handleSearch}
-          className="px-4 py-2 text-sm font-medium text-gray-900 bg-gray-200 border border-gray-200 rounded hover:bg-gray-300">
+          className="s-btn mr-2 px-4 py-2 border border-transparent rounded-lg text-sm font-medium text-gray-600 bg-slate-100 hover:border-gray-400 hover:text-gray-800 focus:outline-none focus:border-gray-400 focus:ring-1 focus:ring-gray-400 cursor-pointer"
+        >
           Google Search
         </button>
         <button
           type="button"
-          className="px-4 py-2 text-sm font-medium text-gray-900 bg-gray-200 border border-gray-200 rounded hover:bg-gray-300">
-          I'm Feeling Lucky
+          className="s-btn px-4 py-2 border border-transparent rounded-lg text-sm font-medium text-gray-600 bg-slate-100 hover:border-gray-400 hover:text-gray-800 focus:outline-none focus:border-gray-400 focus:ring-1 focus:ring-gray-400 cursor-pointer"
+        >
+          I&apos;m Feeling Lucky
         </button>
       </div>
-      </>
+    </>
   );
 };
 
